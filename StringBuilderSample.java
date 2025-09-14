d703b792b1d00d69a36835bce6cb198dc68f878a
Dipa <tamangdipa77@gmail.com>
2025-09-14 22:07:51 +0545

    StringBuilder sample

---
+++ StringBuilderSample.java
@@ -0,0 +1,10 @@
+package DSA;
+
+public class StringBuilderSample {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder("Hello,");
+        System.out.println(builder.toString());
+        builder.append(" Uni:)");
+        System.out.println(builder.toString());
+    }
+}
